46f0b1cd87085b118dff35e6701ebd365a769932
Fatma Galal <fatma.galal@linkdev.com>
2026-02-12 18:48:36 +0200

    wrap login form in SingleChildScrollView

--- lib/src/features/authentcation/presentation/widgets/login_body.dart
+++ lib/src/features/authentcation/presentation/widgets/login_body.dart
@@ -20,70 +20,76 @@ class _LoginBodyState extends State<LoginBody> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Form(
-        key: formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-
-          children: [
-            SizedBox(height: 170, child: Image.asset(AssetsData.logo)),
-
-            SizedBox(height: 24),
-
-            CustomFormTextfield(
-              textFieldHint: 'User Name',
-              onChanged: (data) {
-                email = data;
-              },
-            ),
-
-            SizedBox(height: 12),
-
-            CustomFormTextfield(
-              textFieldHint: 'Password',
-              onChanged: (data) {
-                password = data;
-              },
-              obscarText: true,
-            ),
-
-            SizedBox(height: 24),
-
-            CustomButton(
-              buttonText: 'Login',
-              onTab: () {
-                if (formKey.currentState!.validate()) {
-                 // TODO: Trigger loading indicator, then submit data to API
-                }
-              },
-            ),
-
-            SizedBox(height: 16),
-
-            Row(
+      child: Center(
+        child: SingleChildScrollView(
+           
+        
+          child: Form(
+            key: formKey,
+            child: Column(
+              
               mainAxisAlignment: MainAxisAlignment.center,
+          
               children: [
-                Text(
-                  'You don\'t have an account! ',
-                  style: TextStyle(fontSize: 16, color: kPrimaryColor),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, SignupPage.id);
+                SizedBox(height: 170, child: Image.asset(AssetsData.logo)),
+          
+                SizedBox(height: 24),
+          
+                CustomFormTextfield(
+                  textFieldHint: 'User Name',
+                  onChanged: (data) {
+                    email = data;
                   },
-                  child: Text(
-                    ' Sign-Up ',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: kPrimaryColor,
-                      fontWeight: FontWeight.w600,
+                ),
+          
+                SizedBox(height: 12),
+          
+                CustomFormTextfield(
+                  textFieldHint: 'Password',
+                  onChanged: (data) {
+                    password = data;
+                  },
+                  obscarText: true,
+                ),
+          
+                SizedBox(height: 24),
+          
+                CustomButton(
+                  buttonText: 'Login',
+                  onTab: () {
+                    if (formKey.currentState!.validate()) {
+                     // TODO: Trigger loading indicator, then submit data to API
+                    }
+                  },
+                ),
+          
+                SizedBox(height: 16),
+          
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'You don\'t have an account! ',
+                      style: TextStyle(fontSize: 16, color: kPrimaryColor),
                     ),
-                  ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, SignupPage.id);
+                      },
+                      child: Text(
+                        ' Sign-Up ',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
