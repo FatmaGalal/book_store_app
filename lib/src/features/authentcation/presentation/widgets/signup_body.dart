db5b547e8c843f11274b8f0e4f3459db231c719c
Fatma Galal <fatma.galal@linkdev.com>
2026-02-13 20:32:41 +0200

    sign up exceptions

--- lib/src/features/authentcation/presentation/widgets/signup_body.dart
+++ lib/src/features/authentcation/presentation/widgets/signup_body.dart
@@ -6,6 +6,7 @@ import 'package:book_store/src/core/utils/helpers/show_snak_bar_message.dart';
 import 'package:book_store/src/features/authentcation/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class SignUpBody extends StatefulWidget {
   const SignUpBody({super.key});
@@ -17,99 +18,110 @@ class SignUpBody extends StatefulWidget {
 class _SignUpBodyState extends State<SignUpBody> {
   String? email, password, confirmPassword;
   GlobalKey<FormState> formKey = GlobalKey();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: Center(
-        child: SingleChildScrollView(
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
 
-              children: [
-                SizedBox(height: 170, child: Image.asset(AssetsData.logo)),
+                children: [
+                  SizedBox(height: 170, child: Image.asset(AssetsData.logo)),
 
-                SizedBox(height: 24),
+                  SizedBox(height: 24),
 
-                CustomFormTextfield(
-                  textFieldHint: 'User Name',
-                  onChanged: (data) {
-                    email = data;
-                  },
-                ),
+                  CustomFormTextfield(
+                    textFieldHint: 'User Name',
+                    onChanged: (data) {
+                      email = data;
+                    },
+                  ),
 
-                SizedBox(height: 12),
+                  SizedBox(height: 12),
 
-                CustomFormTextfield(
-                  textFieldHint: 'Password',
-                  onChanged: (data) {
-                    password = data;
-                  },
-                  obscureText: true,
-                ),
+                  CustomFormTextfield(
+                    textFieldHint: 'Password',
+                    onChanged: (data) {
+                      password = data;
+                    },
+                    obscureText: true,
+                  ),
 
-                SizedBox(height: 12),
+                  SizedBox(height: 12),
 
-                CustomFormTextfield(
-                  textFieldHint: 'Confirm Password',
-                  onChanged: (data) {
-                    confirmPassword = data;
-                  },
-                  obscureText: true,
-                ),
+                  CustomFormTextfield(
+                    textFieldHint: 'Confirm Password',
+                    onChanged: (data) {
+                      confirmPassword = data;
+                    },
+                    obscureText: true,
+                  ),
 
-                SizedBox(height: 24),
+                  SizedBox(height: 24),
 
-                CustomButton(
-                  buttonText: 'Sign Up',
-                  onTap: () async {
-                    if (formKey.currentState!.validate()) {
-                      try {
-                        await registerNewUser();
-                      } on FirebaseAuthException catch (firebaseEx) {
-                        if (firebaseEx.code == 'weak-password') {
-                          showMessage(context, 'Weak Password!');
-                        } else if (firebaseEx.code == 'email-already-in-use') {
-                          showMessage(context, 'credential-already-in-use');
+                  CustomButton(
+                    buttonText: 'Sign Up',
+                    onTap: () async {
+                      if (formKey.currentState!.validate()) {
+                      
+                        setState(() {});
+
+                        try {
+                          await registerNewUser();
+                          showMessage(context, 'success!');
+                          Navigator.pushNamed(context, LoginPage.id);
+
+                        } on FirebaseAuthException catch (ex) {
+                          if (ex.code == 'weak-password') {
+                            showMessage(context, 'The password provided is too weak.!');
+                          } else if (ex.code ==  'email-already-in-use') {
+                            showMessage(context, 'The account already exists for that email.');
+                          }
+                        } catch (ex) {
+                          showMessage(context, 'Error!');
                         }
-                      } on Exception catch (e) {
-                        showMessage(context, e.toString());
-                      }
+                   
+                        isLoading = false;
+                        setState(() {});
+                      
+                      } 
+                    },
+                  ),
 
-                      showMessage(context, 'success!');
-                      Navigator.pushNamed(context, LoginPage.id);
-                    }
-                  },
-                ),
+                  SizedBox(height: 16),
 
-                SizedBox(height: 16),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'You already have an account! ',
-                      style: TextStyle(fontSize: 16, color: kPrimaryColor),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, LoginPage.id);
-                      },
-                      child: Text(
-                        ' Login',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: kPrimaryColor,
-                          fontWeight: FontWeight.w600,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'You already have an account! ',
+                        style: TextStyle(fontSize: 16, color: kPrimaryColor),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, LoginPage.id);
+                        },
+                        child: Text(
+                          ' Login',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -119,7 +131,7 @@ class _SignUpBodyState extends State<SignUpBody> {
 
   Future<void> registerNewUser() async {
     var firebaseAuth = FirebaseAuth.instance;
-    var userCredential = await firebaseAuth.createUserWithEmailAndPassword(
+    await firebaseAuth.createUserWithEmailAndPassword(
       email: email!,
       password: password!,
     );
