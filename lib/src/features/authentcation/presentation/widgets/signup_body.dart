b7e8749491eda8101baed388d08722282db6e981
Fatma Galal <fatma.galal@linkdev.com>
2026-02-13 16:41:00 +0200

    handle signup exception

--- lib/src/features/authentcation/presentation/widgets/signup_body.dart
+++ lib/src/features/authentcation/presentation/widgets/signup_body.dart
@@ -2,6 +2,7 @@ import 'package:book_store/src/core/components/custom_button.dart';
 import 'package:book_store/src/core/components/custom_form_textfield.dart';
 import 'package:book_store/src/core/constants/constants.dart';
 import 'package:book_store/src/core/utils/assets_data.dart';
+import 'package:book_store/src/core/utils/helpers/show_snak_bar_message.dart';
 import 'package:book_store/src/features/authentcation/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -16,7 +17,7 @@ class SignUpBody extends StatefulWidget {
 class _SignUpBodyState extends State<SignUpBody> {
   String? email, password, confirmPassword;
   GlobalKey<FormState> formKey = GlobalKey();
-  
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -43,7 +44,6 @@ class _SignUpBodyState extends State<SignUpBody> {
                 SizedBox(height: 12),
 
                 CustomFormTextfield(
-                
                   textFieldHint: 'Password',
                   onChanged: (data) {
                     password = data;
@@ -54,7 +54,6 @@ class _SignUpBodyState extends State<SignUpBody> {
                 SizedBox(height: 12),
 
                 CustomFormTextfield(
-                  
                   textFieldHint: 'Confirm Password',
                   onChanged: (data) {
                     confirmPassword = data;
@@ -68,12 +67,20 @@ class _SignUpBodyState extends State<SignUpBody> {
                   buttonText: 'Sign Up',
                   onTap: () async {
                     if (formKey.currentState!.validate()) {
-                      var firebaseAuth = FirebaseAuth.instance;
-                      var userCredential = await firebaseAuth
-                          .createUserWithEmailAndPassword(
-                            email: email!,
-                            password: password!,
-                          );
+                      try {
+                        await registerNewUser();
+                      } on FirebaseAuthException catch (firebaseEx) {
+                        if (firebaseEx.code == 'weak-password') {
+                          showMessage(context, 'Weak Password!');
+                        } else if (firebaseEx.code == 'email-already-in-use') {
+                          showMessage(context, 'credential-already-in-use');
+                        }
+                      } on Exception catch (e) {
+                        showMessage(context, e.toString());
+                      }
+
+                      showMessage(context, 'success!');
+                      Navigator.pushNamed(context, LoginPage.id);
                     }
                   },
                 ),
@@ -107,6 +114,14 @@ class _SignUpBodyState extends State<SignUpBody> {
           ),
         ),
       ),
+    );
+  }
+
+  Future<void> registerNewUser() async {
+    var firebaseAuth = FirebaseAuth.instance;
+    var userCredential = await firebaseAuth.createUserWithEmailAndPassword(
+      email: email!,
+      password: password!,
     );
   }
 }
