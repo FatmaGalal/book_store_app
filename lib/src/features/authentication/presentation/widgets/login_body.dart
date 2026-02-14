2445f5c79782da8b526738402a160055a4500d7b
Fatma Galal <fatma.galal@linkdev.com>
2026-02-14 16:29:35 +0200

    remove cupertino and add material.dart

--- lib/src/features/authentication/presentation/widgets/login_body.dart
+++ lib/src/features/authentication/presentation/widgets/login_body.dart
@@ -5,8 +5,8 @@ import 'package:book_store/src/core/constants/constants.dart';
 import 'package:book_store/src/core/utils/assets_data.dart';
 import 'package:book_store/src/features/authentication/presentation/pages/signup_page.dart';
 import 'package:book_store/src/features/home/presentation/pages/home_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginBody extends StatefulWidget {
