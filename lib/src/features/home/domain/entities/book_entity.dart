4b7d16af51cb58b87347dd021282fd7f180ce02a
Fatma Galal <fatma.galal@linkdev.com>
2026-02-16 23:17:55 +0200

    add hive to book entity

--- lib/src/features/home/domain/entities/book_entity.dart
+++ lib/src/features/home/domain/entities/book_entity.dart
@@ -1,9 +1,18 @@
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
 class BookEntity {
+@HiveType(typeId: 0)
   final String? bookId;
+@HiveType(typeId: 1)
   final String? kind;
+@HiveType(typeId: 2)
   final String? title;
+@HiveType(typeId: 3)
   final String? authors;
+@HiveType(typeId: 4)
   final String? categories;
+@HiveType(typeId: 5)
   final String? imageLink;
 
   const BookEntity({
