15c0bfed0a94277c97c8637a10ad7ee131cdd045
Fatma Galal <fatma.galal@linkdev.com>
2026-02-16 21:19:32 +0200

    use either class

--- lib/src/features/home/domain/repos/home_repo.dart
+++ lib/src/features/home/domain/repos/home_repo.dart
@@ -1,5 +1,7 @@
+import 'package:book_store/src/core/errors/failure.dart';
 import 'package:book_store/src/features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchBookList();
+  Future<Either<Failure, List<BookEntity>>> fetchBookList();
 }
