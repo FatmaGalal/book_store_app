5a333ab1e2dfa6a5f9e06df4a198f1d86bd96d33
Fatma Galal <fatma.galal@linkdev.com>
2026-02-19 15:09:14 +0200

    signin unit test

--- lib/src/features/authentication/presentation/providers/auth_providers.dart
+++ lib/src/features/authentication/presentation/providers/auth_providers.dart
@@ -1,6 +1,7 @@
 import 'package:book_store/src/features/authentication/data/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authServiceProvider = Provider<AuthService>((ref) {
-  return AuthService();
+  return AuthService(auth: FirebaseAuth.instance);
 });
